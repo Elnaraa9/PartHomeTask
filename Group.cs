4278b310c383b6eeb9131010badf9cc3b08ee319
Elnaraa9 <100670042+Elnaraa9@users.noreply.github.com>
2022-03-30 12:24:28 +0400

    Add files via upload

---
+++ Group.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccessModifiers.Models
+{
+    class Group
+    {
+        public string Name { get; set; }
+        public int MaxStuCount { get; set; }
+        public int ExamPoint;
+        public int[] CurrentStudent;
+        public int[] NewStudent;
+        public Student[] Students { get; set; }
+
+        public Group(string name, int maxStuCount, params Student[] students)
+        {
+            Name = name;
+            MaxStuCount = maxStuCount;
+            Students = students;
+
+        }
+        public void RemoveStudent(Student students)
+        {
+            if (ExamPoint <= 81)
+            {
+                Console.WriteLine($"Student is removed, MaxStuCount-{MaxStuCount}");
+            }
+            Console.WriteLine("Student is successed");
+        }
+        public void AddStudent()
+        {
+            if (MaxStuCount > CurrentStudent.Length + NewStudent.Length)
+            {
+                MaxStuCount++;
+                Console.WriteLine($"New student is accepted, New count-{MaxStuCount} ");
+            }
+            Console.WriteLine("Limit is overrated");
+             foreach (var item in Students)
+	         {
+              Console.WriteLine(Students);
+             }
+        }
+    }   
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Group student = new Group();
+            student.Name = "Elnara";
+            student.Surname = "Vahabova";
+
+            student.AddStudent();
+            student.RemoveStudent();
+            Console.WriteLine(student.Name,student.Surname,student);
+        }
+    }
+}
